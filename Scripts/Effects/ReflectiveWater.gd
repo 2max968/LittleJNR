ad57f1aafbcafa01fb8a22093b92329983acdf19
2max968 <62080624+2max968@users.noreply.github.com>
2023-04-18 23:38:56 +0200

    ...

--- Scripts/Effects/ReflectiveWater.gd
+++ Scripts/Effects/ReflectiveWater.gd
@@ -5,17 +5,20 @@ export var reflection_scale := 0.75;
 export var water_color := Color(0, 0.670588, 1, 0.4)
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 func _process(delta):
 	
 	if Engine.editor_hint:
-		material.set_shader_param("y_zoom", get_viewport().global_canvas_transform.y.y / reflection_scale)
+		material.set_shader_param("y_zoom", get_viewport().global_canvas_transform.y.y)
 	else:
-		material.set_shader_param("y_zoom", get_viewport_transform().get_scale().y / reflection_scale)
+		material.set_shader_param("y_zoom", get_viewport_transform().get_scale().y)
 	material.set_shader_param("color", water_color)
+	material.set_shader_param("reflection_scale", reflection_scale)
 	
 
 func _on_Water_item_rect_changed():
+	print("Set scale to: " + str(scale))
 	material.set_shader_param("scale", scale)
+		
