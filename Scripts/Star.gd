de5491d01584b9eae627e59d5dabd1bc9a816f25
2max968 <62080624+2max968@users.noreply.github.com>
2023-08-24 20:01:41 +0200

    Star new sprite and animation

--- Scripts/Star.gd
+++ Scripts/Star.gd
@@ -3,13 +3,15 @@ extends Area2D
 const KEEP_DISTANCE := 32;
 
 var following : bool = false
+var animation : float = 0
 
 func _init():
 	add_to_group("levelControl")
 
 func _on_Star_body_entered(body):
-	if body is Player_Base:
+	if body is Player_Base and not following:
 		following = true
+		animation = 2
 		
 func _process(delta):
 	if following:
@@ -20,6 +22,13 @@ func _process(delta):
 			if distance > KEEP_DISTANCE:
 				var direction = (target - global_position).normalized() * delta * (distance - KEEP_DISTANCE) * 3
 				global_position += direction
+	
+	if animation > 0:
+		var sx := cos((2 - animation*animation)*2*PI)
+		$Sprite.scale.x = sx
+		animation -= delta
+	else:
+		$Sprite.scale.x = 1
 
 func finishLevel(nextScene : String):
 	if following:
