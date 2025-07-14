6324aba83e7c71221363c0f01c787fa77e810e26
2max968 <62080624+2max968@users.noreply.github.com>
2025-07-15 00:09:10 +0200

    Add crystals background model and update camera logic
    Added Crystals.glb model and associated materials to the background. Updated Level04 scene to include the new crystals, a DirectionalLight, and adjusted transforms. Modified CameraBgr3D.gd to use a vertical limit node for improved camera positioning.

--- Scripts/CameraBgr3D.gd
+++ Scripts/CameraBgr3D.gd
@@ -2,13 +2,15 @@ extends Camera
 
 export var MovementFactor := 1.0
 var Cam2D: Camera2D
+var vlimit: Node2D
 
 func _ready():
 	Cam2D = get_tree().get_root().find_node("Camera", true, false)
+	vlimit = get_tree().get_root().find_node("VLimit", true, false)
 	pause_mode = Node.PAUSE_MODE_PROCESS
 	process_priority = -10
 	
 func _process(delta):
 	if Cam2D != null:
 		var pos2D := Cam2D.get_camera_screen_center()
-		global_position = Vector3(pos2D.x, -pos2D.y, 0) / 512 * MovementFactor
+		global_position = Vector3(pos2D.x, vlimit.global_position.y - pos2D.y, 0) / 512 * MovementFactor
